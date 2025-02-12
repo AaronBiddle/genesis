672c81a3e169f907227415b2757b66a48fa61eea
Aaron Biddle <biddlea@gmail.com>
2025-02-12 05:47:43 -0500

    created a user-data folder

--- backend/backend-python/l1_create_docker.sh
+++ backend/backend-python/l1_create_docker.sh
@@ -16,6 +16,7 @@ echo "Starting new container..."
 docker run -it --name python-backend \
     -p 8000:8000 \
     -v $(pwd)/app:/app \
+    -v $(pwd)/../user-data:/app/user-data \
     python-backend bash
 
 echo "If you see this message, the container exited unexpectedly."
