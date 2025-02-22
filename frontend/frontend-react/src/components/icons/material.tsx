0b7f3a10a3f49c50612dfdcc2d1e3c9035dc95f9
Aaron Biddle <biddlea@gmail.com>
2025-02-22 11:31:58 -0500

    ***Switched to new frontend***

--- frontend/frontend-react/src/components/icons/material.tsx
+++
@@ -1,9 +0,0 @@
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
-import SaveIcon from '@mui/icons-material/Save';
-import SaveAsIcon from '@mui/icons-material/SaveAs';
-
-export const MaterialIcons = {
-  NoteAdd: NoteAddIcon,
-  Save: SaveIcon,
-  SaveAs: SaveAsIcon,
-}; 
