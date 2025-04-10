2ac367c4ea22d8dafb58317eb62bdbed9438b765
Aaron Biddle <biddlea@gmail.com>
2025-04-10 04:00:48 -0400

    ...

--- frontend/src/components/DocumentEditor/DocumentEditor.vue
+++ frontend/src/components/DocumentEditor/DocumentEditor.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="p-2 pt-0 h-full w-full flex flex-col">
     <div class="toolbar flex items-center p-1 mb-0">
-      <button class="p-1 hover:bg-gray-200 rounded">
+      <button class="p-1 hover:bg-gray-200 rounded" @click="openFileManager('open')">
         <img src="@/components/Icons/icons8/icons8-open-file-80.png" alt="Open" class="h-6 w-6">
       </button>
-      <button class="p-1 hover:bg-gray-200 rounded ml-1">
+      <button class="p-1 hover:bg-gray-200 rounded ml-1" @click="openFileManager('save')">
         <img src="@/components/Icons/icons8/icons8-save-80.png" alt="Save" class="h-6 w-6">
       </button>
-      <button class="p-1 hover:bg-gray-200 rounded ml-1">
+      <button class="p-1 hover:bg-gray-200 rounded ml-1" @click="openFileManager('save')">
         <img src="@/components/Icons/icons8/icons8-save-as-80.png" alt="Save As" class="h-6 w-6">
       </button>
     </div>
@@ -20,9 +20,19 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, inject } from 'vue';
+import { addWindow } from '@/components/WindowSystem/WindowManager';
+import { apps } from '@/components/WindowSystem/apps';
 
 const content = ref('');
+const windowId = inject('windowId', -1);
+
+function openFileManager(mode: 'open' | 'save' | 'none') {
+  const fileManagerApp = apps.find(app => app.id === 'file-manager');
+  if (fileManagerApp) {
+    addWindow(fileManagerApp, { parentId: windowId });
+  }
+}
 </script>
 
 <style scoped>
