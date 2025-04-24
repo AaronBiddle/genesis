7588284584b618c3026c01c0fac713f43419cc6d
Aaron Biddle <biddlea@gmail.com>
2025-04-24 11:06:16 -0400

    ...

--- frontend/src/components/Icons/IconViewer.vue
+++ frontend/src/components/Icons/IconViewer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="p-4 bg-white h-full overflow-y-auto">
-    <ul class="grid grid-cols-2 gap-4">
+    <ul class="grid grid-cols-2">
       <li v-for="[id, iconHtml] in svgIcons" :key="id" class="flex items-center pb-1">
         <span class="icon-preview w-8 h-8 mr-3 flex items-center justify-center" v-html="iconHtml"></span>
         <span class="font-mono text-sm">{{ id }}</span>
